c0c76a52d8865db270c5bab7410f57e16b8d5d9d
A1i5h8a <>
2021-04-06 16:29:05 +0000

    fixed gamestart error

--- main.py
+++ main.py
@@ -9,37 +9,27 @@ red=(255,0,0)
 screenWidth=400
 screenHeight=250
 fps=15
+gameStart = True
 screen= pygame.display.set_mode((screenWidth,screenHeight))
 pygame.display.set_caption('snake-game')
 clock=pygame.time.Clock()
 font=pygame.font.Font(None, 25)
+
+
 def gameOver_message(msg, textcolor):
   text= font.render(msg, True, textcolor)
   screen.blit(text, [10, screenHeight/2])
-def gameloop(): 
-  gameStart = True
+
+
+def gameloop(gameStart): 
   gameOver=False
   movex=0
   movey=0
   x=100
   y=100
   block_size=5
+  c=0
   while gameStart:
-    #controlling game Over
-    while gameOver==True:
-      screen.fill(white)
-      gameOver_message("Game Over! press c to continue and q to quit", red)
-      pygame.display.update()
-      for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-          if event.key == pygame.K_q:
-            print('quit')
-            gameOver=False
-            gameStart=False
-          elif event.key == pygame.K_c:
-            print('game running')
-            gameloop()
-        
     #controling Quit
     for event in pygame.event.get():
       if event.type== pygame.QUIT:
@@ -60,9 +50,6 @@ def gameloop():
           movex= 0
           movey = +5
       #defining borders
-      
-
-
       '''
       if event.type == pygame.KEYUP:
         if event.key == pygame.K_LEFT or event.key==pygame.K_RIGHT:
@@ -77,7 +64,29 @@ def gameloop():
     screen.fill(white)
     pygame.draw.rect(screen, black,[x,y,block_size,block_size])
     pygame.display.update()
+       #controlling game Over
+    while gameOver==True:
+      screen.fill(white)
+      c += 1
+      print(f'count{c}')
+      gameOver_message("Game Over! press c to continue and q to quit", red)
+      pygame.display.update()
+      for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_c:
+            print('game running')
+            gameloop(gameStart)
+            gameOver=False
+            gameStart=False
+          elif event.key == pygame.K_q:
+            print('quit')
+            gameOver=False
+            gameStart=False  
+        if event.type== pygame.QUIT:
+          pygame.quit
+          gameOver=False
+          gameStart = False       
   pygame.quit
     #x= x + movex
-gameloop()
+gameloop(gameStart)
 
